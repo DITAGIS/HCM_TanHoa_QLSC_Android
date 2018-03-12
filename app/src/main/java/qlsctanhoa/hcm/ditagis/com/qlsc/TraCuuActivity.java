bd3663852ef2367266ce791231d0834e78c78824
thanle95 <lengocthan95@gmail.com>
2018-03-12 11:51:07 +0700

    listview tra cuu

--- app/src/main/java/qlsctanhoa/hcm/ditagis/com/qlsc/TraCuuActivity.java
+++ app/src/main/java/qlsctanhoa/hcm/ditagis/com/qlsc/TraCuuActivity.java
@@ -8,16 +8,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+
+import qlsctanhoa.hcm.ditagis.com.qlsc.adapter.TraCuuAdapter;
 
 public class TraCuuActivity extends AppCompatActivity {
+
+    private TraCuuAdapter mTraCuuAdapter;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tra_cuu);
+        List<TraCuuAdapter.Item> items = new ArrayList<>();
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 1, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 0, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 1, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 1, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 2, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+        items.add(new TraCuuAdapter.Item("01_12_03_2018", 1, "12/03/2018", "327 Lê Văn Phương, Phường Tân Quy, Quận 7, Thành Phố Hồ Chí Minh"));
+
+        this.mTraCuuAdapter = new TraCuuAdapter(TraCuuActivity.this, items);
+
+        this.mListView = findViewById(R.id.lstView_TraCuu);
+        this.mListView.setAdapter(this.mTraCuuAdapter);
     }
 
     public void showDateTimePicker(View view) {
@@ -49,11 +69,12 @@ public class TraCuuActivity extends AppCompatActivity {
 
 
     }
+
     @Override
     public void finish() {
         Intent returnIntent = new Intent();
-        returnIntent.putExtra("result",3);
-        setResult(Activity.RESULT_OK,returnIntent);
+        returnIntent.putExtra("result", 3);
+        setResult(Activity.RESULT_OK, returnIntent);
 
         super.finish();
     }
---
+++ app/src/main/java/qlsctanhoa/hcm/ditagis/com/qlsc/adapter/TraCuuAdapter.java
@@ -0,0 +1,150 @@
+package qlsctanhoa.hcm.ditagis.com.qlsc.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import qlsctanhoa.hcm.ditagis.com.qlsc.R;
+
+/**
+ * Created by ThanLe on 04/10/2017.
+ */
+
+public class TraCuuAdapter extends ArrayAdapter<TraCuuAdapter.Item> {
+    private Context context;
+    private List<Item> items;
+
+    public TraCuuAdapter(Context context, List<Item> items) {
+        super(context, 0, items);
+        this.context = context;
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void clear() {
+        items.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+//    public Item getItem(String mlt) {
+//        for (Item item : this.items)
+//            if (item.getTieuThu().equals(mlt))
+//                return item;
+//        return null;
+//    }
+//
+//    public boolean removeItem(String mlt) {
+//        for (Item item : this.items)
+//            if (item.getTieuThu().equals(mlt)) {
+//                this.items.remove(item);
+//                return true;
+//            }
+//        return false;
+//    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_tracuu, null);
+        }
+        Item item = items.get(position);
+
+        LinearLayout layout = (LinearLayout) convertView.findViewById(R.id.layout_tracuu);
+        switch (item.getTrangThai()) {
+            //chưa sửa chữa
+            case 0:
+                layout.setBackgroundColor(ContextCompat.getColor(context, R.color.color_chua_sua_chua));
+                break;
+            //đã sửa chữa
+            case 1:
+                layout.setBackgroundColor(ContextCompat.getColor(context, R.color.color_da_sua_chua));
+                break;
+            //đang sửa chữa
+            case 2:
+                layout.setBackgroundColor(ContextCompat.getColor(context, R.color.color_dang_sua_chua));
+                break;
+        }
+
+        TextView txtID = (TextView) convertView.findViewById(R.id.txt_tracuu_id);
+        //todo
+        txtID.setText(item.getId());
+
+        TextView txtDiaChi = (TextView) convertView.findViewById(R.id.txt_tracuu_diachi);
+        //todo
+        txtDiaChi.setText(item.getDiaChi());
+
+        TextView txtNgayCapNhat = (TextView) convertView.findViewById(R.id.txt_tracuu_ngaycapnhat);
+        //todo
+        txtNgayCapNhat.setText(item.getNgayCapNhat());
+
+
+        return convertView;
+    }
+
+    public static class Item {
+        String id;
+        int trangThai;
+        String ngayCapNhat;
+        String diaChi;
+
+        public Item(String id, int trangThai, String ngayCapNhat, String diaChi) {
+            this.id = id;
+            this.trangThai = trangThai;
+            this.ngayCapNhat = ngayCapNhat;
+            this.diaChi = diaChi;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public int getTrangThai() {
+            return trangThai;
+        }
+
+        public void setTrangThai(int trangThai) {
+            this.trangThai = trangThai;
+        }
+
+        public String getNgayCapNhat() {
+            return ngayCapNhat;
+        }
+
+        public void setNgayCapNhat(String ngayCapNhat) {
+            this.ngayCapNhat = ngayCapNhat;
+        }
+
+        public String getDiaChi() {
+            return diaChi;
+        }
+
+        public void setDiaChi(String diaChi) {
+            this.diaChi = diaChi;
+        }
+    }
+}
